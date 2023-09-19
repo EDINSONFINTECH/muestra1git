60fdbd329f7a13103e39fa3bb77b14cf410618d1
EdinsonFintech <edinsonfintech@gmail.com>
2023-09-19 13:01:07 -0500

    Seguimos guardando las pruebas

--- muestra.py
+++ muestra.py
@@ -1,3 +1,4 @@
 saludar = "Soy el mejor desarrolador"
 print(saludar)
 print(" Muestra algo que podemos hacer  ")
+# se puede Agregar siempre nuevos comandos
--- muestra2.py
+++ muestra2.py
@@ -1,3 +1,3 @@
 # Aqui se muestra otro Archivo nuevo
 otro_saludo = "Aqui muestro como Avanzo"
-print("Esto muestra el Avance")
+print("Esto muestra el Avance cada dia en progreso")
