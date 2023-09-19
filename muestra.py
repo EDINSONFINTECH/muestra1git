498c42cb17fed20c8e349bc4eb723cd5c8c15a75
EdinsonFintech <edinsonfintech@gmail.com>
2023-09-19 13:42:50 -0500

    Actualizacion de verificacion EQ

--- muestra.py
+++ muestra.py
@@ -2,3 +2,5 @@ saludar = "Soy el mejor desarrolador"
 print(saludar)
 print(" Muestra algo que podemos hacer  ")
 # se puede Agregar siempre nuevos comandos
+# Esto muestra muchas mejoras en el Desarrollo
+print("saludos de nuevo")
